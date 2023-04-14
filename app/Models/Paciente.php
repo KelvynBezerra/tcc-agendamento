4f303ac5160d55881154cacdeb05d3f9e043c91c
ErIn4moto <erick.durval@etec.sp.gov.br>
2023-04-13 21:08:19 -0300

    banco de dadaismo

--- app/Models/Paciente.php
+++ app/Models/Paciente.php
@@ -4,10 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 class Paciente extends Model
 {
     use HasFactory;
     protected $table="paciente";
     public $timestamps = false;
+
+    public function Consultas(): HasMany
+    {
+        return $this->hasMany(Consulta::class);
+    }
+
+    public function Medico(): HasOneThrough
+    {
+        return $this->hasOneThrough(Medico::class, Consulta::class);
+    }
+
+
 }
+
+    
--- resources/views/home-cliente.blade.php
+++ resources/views/home-cliente.blade.php
@@ -1,1 +1,9 @@
 <h1>home cliente</h1>
+
+{{-- use App\Models\Paciente;
+ 
+$HistConsultas = consultas::all();
+ 
+foreach ($HistConsultas as $consulta) {
+    echo $HistConsultas->consulta->data;
+} --}}
