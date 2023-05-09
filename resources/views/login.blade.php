542cb4303573b35b94460b312ebbb60ac5b4b42c
KelvynBezerra <kekelvynhooliveira@gmail.com>
2023-05-08 21:40:05 -0300

    Atualizacao

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/estilizacao.css') }}">
-    <title>Login</title>    
+    <title>Login</title>    <link rel="stylesheet" type="text/css" href="{{ asset('css/estilizacao.css') }}">
 </head>
 <body>
         <header>
@@ -31,8 +30,6 @@
 
                 <input type="submit" value="Entrar" class="enviar-login"> <br>
                 <span class="texto-cadastrar"> Não tem conta? <span><a href="/cadastro">Cadastre-se</a></span></span>
-
-                
 
                 @csrf
 
