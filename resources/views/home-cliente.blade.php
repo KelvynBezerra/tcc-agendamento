03e3270f98ad9d1be6c7a83f493b17ff9c3bdf9f
Erinamoto <132098231+kodivani@users.noreply.github.com>
2023-06-15 21:22:30 -0300

    quase acabou

--- resources/views/home-cliente.blade.php
+++ resources/views/home-cliente.blade.php
@@ -1,99 +1,70 @@
 <x-layout>
-
-    <span class="text-Indicator space-40">Consultas</span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <a href="exames"><div class="button primary " style="margin-top: 40px; margin-right: -175px"> NOVO EXAME</div></a>
-        <a href="horario"><div class="button primary " style="margin-top: 40px;"> NOVA CONSULTA</div></a>
-        
-
-    <span></span>
-
-
+  <span class="text-Indicator space-40">Consultas</span>
+  <span></span>
+  <span></span>
+  <span></span>
+  <a href="exames"><div class="button primary " style="margin-top: 40px; margin-right: -175px"> NOVO EXAME</div></a>
+  <a href="horario"><div class="button primary " style="margin-top: 40px;"> NOVA CONSULTA</div></a>
+  <span></span>
   </div>
   <center>
     <div class="agrupamentoCards content-center">
       <div class=" flex-row content-center gap-40">
-      @foreach ($consultas as $consulta)
-        @if($consulta->id_tipoAgendamento="1")
-          <div class="cards">
-         
-            <div class="flex-row space-10">
-              <span class="material-icons">
-                schedule
-              </span>
-              <span>{{ strftime('%d/%m/%Y %H:%M', strtotime($consulta->data_hora)) }}</span>
-            </div>
-          <div class="flex-row space-10">
-            <span class="material-icons">
-              vaccines
-            </span>
-            <span style="font-size: 20px;">{{$consulta?->medico?->especialidade }}</span>
-            <div class="break">{{ $consulta?->medico?->nome }}</div>
-          
-          <div style="margin-left:20px">
-            <button type="sub" class="button">Excluir consulta</button>
-          </div>
-          
+          @foreach ($consultas as $consulta)
+            @if($consulta->id_tipoAgendamento=="1")
+              <div class="cards">         
+                <div class="flex-row space-10">
+                  <span class="material-icons">
+                    schedule
+                  </span>
+                  <span>{{ strftime('%d/%m/%Y %H:%M', strtotime($consulta->data_hora)) }}</span>
+                </div>
+                <div class="flex-row space-10">
+                  <span class="material-icons">
+                    vaccines
+                  </span>
+                  <span style="font-size: 20px;">{{$consulta?->medico?->especialidade }}</span>
+                  <div class="break">{{ $consulta?->medico?->nome }}</div>
+                  <div style="margin-left:20px">
+                    <a href="/cancelar/{{$consulta->id}}"><div class="button primary " style="margin-top: 40px">Cancelar consulta</div></a>
+                  </div>
+                </div>
+                <div class="break"></div> <br>
+                {{-- Fim do cartão  --}}
+              </div>
+              {{-- Espaço entre cartão --}}
 
-          </div>
-          <div class="break"></div> <br>
+              <!-- EXAMES-->
+            @else
 
-        </div>
-      </div>
-    </div>
-    
-
-        <!-- EXAMES-->
-         @else
-        <div class="agrupamentoCards content-center">
-      <div class=" flex-row content-center gap-40">
-        <div class="cards">
-          <h1>1</h1>
-          <div class="flex-row space-10">
-            
-            <span>10/04/2023 10:00</span>
-          </div>
-          <span class="material-symbols-outlined">
-              prescriptions
-            </span>
-        </div>
-        
-        <div class="agrupamentoCards content-center">
-      <div class=" flex-row content-center gap-40">
-        <div class="cards">
-          <h1>1</h1>
-          <div class="flex-row space-10">
-            <span class="material-icons">
-              schedule
-            </span>
-            <span>10/04/2023 10:00</span>
-          </div>
-
-          <div class="flex-row space-10">
-            <span class="material-symbols-outlined">
-              prescriptions
-            </span>
-            <span style="font-size: 20px;">blabla</span>
-            <div style="margin-left:20px">
-          <button type="sub" class="button" style="float: right">Excluir Exame</button>
-          </div>
-          
-          </div>
-          
-        </div>
-        
-        @endif
-        @endforeach
-        
-        
+              <div class="cards">         
+                <div class="flex-row space-10">
+                  <span class="material-icons">
+                    schedule
+                  </span>
+                  <span>{{ strftime('%d/%m/%Y %H:%M', strtotime($consulta->data_hora)) }}</span>
+                </div>
+                <div class="flex-row space-10">
+                  <span class="material-icons">
+                    vaccines
+                  </span>
+                  <span style="font-size: 20px;">{{ $consulta?->exame }}</span>
+                  
+                  <div style="margin-left:20px">
+                    
+                    <a href="/cancelar/{{$consulta->id}}"><div class="button primary " style="margin-top: 40px;">Cancelar exame</div></a>
+                    
+                  </div>
+                </div>
+                <div class="break"></div> <br>
+                {{-- Fim do cartão  --}}
+              </div>
+              {{-- Espaço entre cartão --}}
 
 
-      </div>
+
+            @endif
+          @endforeach
 
   </center>  
-
-  
-
 </x-layout>
